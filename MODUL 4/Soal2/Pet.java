9c960874c01d2e532ff555848343ea63d6632fe4
Fadhil Syahdama Mahatma Putra <fadatra35@gmail.com>
2025-10-11 12:24:50 +0700

    Update Pet.java

--- MODUL 4/Soal2/Pet.java
+++ MODUL 4/Soal2/Pet.java
@@ -1,18 +1,17 @@
 package Soal2;
+
 public class Pet {
-    String name;
-    String race;
+    private String name;
+    private String race;
 
-    public Pet(){}
-
-    public void pet(String r, String n){
+    public Pet(String r, String n) {
         this.race = r;
         this.name = n;
     }
 
-    public void display(){
+    public void display() {
         System.out.println("Detail Hewan Peliharaan:");
-        System.out.println("Nama hewan peliharaanku adalah: "+name);
-        System.out.println("Dengan ras: "+race);
+        System.out.println("Nama hewan peliharaanku adalah: " + this.name);
+        System.out.println("Dengan ras: " + this.race);
     }
 }
