cdd60e0ed5d9162b7b1fddbe8e85b3cd1da235b7
Fadhil Syahdama Mahatma Putra <fadatra35@gmail.com>
2025-10-11 12:25:20 +0700

    Update Main.java

--- MODUL 4/Soal2/Main.java
+++ MODUL 4/Soal2/Main.java
@@ -6,21 +6,44 @@ public class Main{
 
     public static void main(String[] args){
 
-        Cat myCat = new Cat();
-
         System.out.println("Pilih jenis hewan yang ingin diinputkan:");
         System.out.print("1 = Kucing\n");
         System.out.print("2 = Anjing\n");
         System.out.print("Masukkan pilihan: ");
         int choice = scan.nextInt();
+
         if(choice == 1){
-            myCat.catInput();
-            myCat.display();
-            myCat.fur();
+            catData();
         } else if(choice == 2){
-
+            dogData();
         }
+    }
 
+    static private void catData(){
+        Main.scan.nextLine();
+        System.out.print("Nama hewan peliharaan: ");
+        String name = scan.nextLine();
+        System.out.print("Ras: ");
+        String race = scan.nextLine();
+        System.out.print("Warna Bulu: ");
+        String furColor = scan.nextLine();
 
+        Cat cat = new Cat(race, name, furColor);
+        cat.displayCatDetail();
+    }
+
+    static private void dogData(){
+        Main.scan.nextLine();
+        System.out.print("Nama hewan peliharaan: ");
+        String name = scan.nextLine();
+        System.out.print("Ras: ");
+        String race = scan.nextLine();
+        System.out.print("Warna Bulu: ");
+        String furColor = scan.nextLine();
+        System.out.print("Kemampuan: ");
+        String skill  = scan.nextLine();
+
+        Dog dog = new Dog(name, race, furColor, skill);
+        dog.displayDogDetail();
     }
 }
