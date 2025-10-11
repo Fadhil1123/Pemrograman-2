59f1aa3ea6ea74fa112f516d94f7ac8fa728aab0
Fadhil Syahdama Mahatma Putra <fadatra35@gmail.com>
2025-10-11 12:19:19 +0700

    Update Main.java

--- MODUL 4/Soal1/Main.java
+++ MODUL 4/Soal1/Main.java
@@ -1,20 +1,17 @@
 package Soal1;
-
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        pet myPet = new pet("", "");
 
-        myPet.input1();
+        System.out.print("Nama: ");
         String name = scan.nextLine();
 
-        myPet.input2();
+        System.out.print("Ras: ");
         String race = scan.nextLine();
 
-        myPet = new pet(race, name);
-
+        pet myPet = new pet(race, name);
         myPet.display();
         scan.close();
     }
