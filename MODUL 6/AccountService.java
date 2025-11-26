5ba30f60d3f057995d3db2d51a7390017eb92d77
Fadhil Syahdama Mahatma Putra <fadatra35@gmail.com>
2025-11-26 19:10:00 +0700

    Delete MODUL 6 directory

--- MODUL 6/AccountService.java
+++
@@ -1,8 +0,0 @@
-package LoginSystem;
-import java.util.*;
-
-public interface AccountService {
-    List<String> ListUsernames();
-    AccountRepository.Account authenticate(String username, String password);
-    public void register(String uername, String password);
-}
--- MODUL 6/MenuAction.java
+++
@@ -1,6 +0,0 @@
-package LoginSystem;
-
-public interface MenuAction {
-    public void execute();
-    public String name();
-}
